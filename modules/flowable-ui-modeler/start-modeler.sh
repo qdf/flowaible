bb44109a64bbe6c04c19db842752d5c4c3cbbc36
Cloud User <centos@ip-172-31-22-213.ec2.internal>
2019-07-12 06:49:44 +0000

    Updated logo files

--- modules/flowable-ui-modeler/start-modeler.sh
+++ modules/flowable-ui-modeler/start-modeler.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "Building all modeler ui submodules"
+export FLOWABLE_COMMON_APP_IDM_URL="http://a.keera.ai:8080/flowable-idm"
 mvn -T 1C clean install -DskipTests
 STATUS=$?
 if [ $STATUS -eq 0 ]
@@ -8,7 +9,7 @@ then
 
 	# Run war
 	export MAVEN_OPTS="$MAVEN_OPTS -noverify -Xms512m -Xmx1024m -Xdebug -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=n"
-	mvn clean install -Pmysql -DskipTests spring-boot:run
+	mvn clean install -DskipTests spring-boot:run
 else
     echo "Error while building root pom. Halting."
 fi
